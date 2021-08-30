c327941542d602a0f3d07520d51be4709a42b302
Sarah <sarahlchavez@gmail.com>
2021-08-30 05:23:31 -0700

    commit tree 07c1f82f18b87de24edb4e47f1af1c0a896150fa

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+document.getElementById('main').remove();
+
+// add a newHeader variable h1#victory
+let newHeader = document.createElement('h1');
+newHeader.id = "victory"
+newHeader.innerHTML = "Sarah is the champion"
+
